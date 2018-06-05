14b0e6f17533d7ba40aa74799d511038cc03215e
MihailTS <tseluiko.m@gmail.com>
2018-06-05 08:33:28 +0300

    clear Visitor model

--- app/Visitor.php
+++ app/Visitor.php
@@ -2,21 +2,12 @@
 
 namespace App;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 class Visitor extends Model
 {
     protected $fillable = [
         'hash'
     ];
-
-
-    /*public function getCreatedAtColumn($value)
-    {
-        $c = new Carbon($value);
-        return $c->setTimezone('Europe/Moscow');
-    }*/
-
 
     public function visits(){
         return $this->hasMany(Visit::class);
